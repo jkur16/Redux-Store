20e63aa6e7aaa71990fac16db91dc8f04b51e3de
Jamie Kuras <jamiekuras@gmail.com>
2021-09-18 15:51:41 -0400

    update connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jkur16:Dec261983*@cluster0.cop4i.mongodb.net/ReduxStore?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
